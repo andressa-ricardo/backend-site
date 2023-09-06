15ae2d17c8ea85765856195dac35c20d962d4c0e
andressa-ricardo <andressaricardo30@gmail.com>
2023-09-06 13:42:17 -0300

    feat: add productController to server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import productController from "./controllers/ProductController";
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(productController)
 
 app.get("/", (req, res) => {
   res.send("rodando");
