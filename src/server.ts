6597bd44ee83259f16ffdca514fcd1c204ee2274
andressa-ricardo <andressaricardo30@gmail.com>
2023-09-13 13:55:09 -0300

    feat: rotas adicionadas no server

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,22 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import productController from "./controllers/ProductController";
 import userController from "./controllers/UserController";
+import dotenv from "dotenv"
 
 const app = express();
 const PORT = 3000;
+dotenv.config()
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(productController)
 app.use(userController)
+
+export const auth = {
+  secret: String(process.env.SECRET),
+  expires: '1h',
+};
 
 app.get("/", (req, res) => {
   res.send("rodando");
