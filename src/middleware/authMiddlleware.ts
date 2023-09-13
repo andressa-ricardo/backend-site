d286f4dcc6557f2819a46c2627ce56b91454d6a8
andressa-ricardo <andressaricardo30@gmail.com>
2023-09-13 13:53:56 -0300

    feat: jwt implementado

---
+++ src/middleware/authMiddlleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+
+export default function authMiddlleware(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers.authorization) {
+    return res.sendStatus(401);
+  }
+  const [bearer, token] = req.headers.authorization?.split(" ");
+  console.log(bearer, token);
+  if (bearer !== "Bearer") {
+    return res.sendStatus(401);
+  }
+  try {
+    jwt.verify(token, process.env.SECRET as string);
+  } catch {
+    return res.sendStatus(401);
+  }
+  next()
+}
